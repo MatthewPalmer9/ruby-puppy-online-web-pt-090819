917fdc0f07786fcffea3fed28417af190b6a24dc
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-10-03 22:32:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,26 @@
-# Add your code here
+class Dog
+  attr_accessor :name 
+  
+  @@all = [] 
+  
+  def initialize(name)
+    @name = name
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def self.save
+    @@all << self 
+  end 
+  
+  def self.clear_all
+    @@all.clear()
+  end 
+  
+  def self.print_all
+    @@all.sort_by(){|dog| puts dog.name}
+  end 
+end 
